56ba8c27be9829a9a5cb18126d684cdbd36f4490
heisenbug3 <heisenbug30@gmail.com>
2020-11-03 21:44:03 -0500

    Initial commit

---
+++ src/test/java/com/heisenbug3/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heisenbug3.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
