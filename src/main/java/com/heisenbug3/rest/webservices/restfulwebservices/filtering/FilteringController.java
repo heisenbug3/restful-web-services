0c5460bd537fe8b69ce6998f04546360162d5553
heisenbug3 <heisenbug30@gmail.com>
2020-11-08 15:32:26 -0500

    Dynamic filtering

--- src/main/java/com/heisenbug3/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/heisenbug3/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,13 +1,43 @@
 package com.heisenbug3.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FilteringController {
 
 	@GetMapping("/filtering")
-	public SomeBean retrieveSomeBean() {
-		return new SomeBean("", "", "");
+	public MappingJacksonValue retrieveSomeBean() {
+		SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		mapping.setFilters(filters);
+		return mapping;
+	}
+
+	@GetMapping("/filtering-list")
+	public MappingJacksonValue retrieveListOfSomeBeans() {
+		SomeBean someBean1 = new SomeBean("value1", "value2", "value3");
+		SomeBean someBean2 = new SomeBean("value11", "value22", "value33");
+		List<SomeBean> beanList = List.of(someBean1, someBean2);
+
+		MappingJacksonValue mapping = new MappingJacksonValue(beanList);
+
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field3");
+
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		mapping.setFilters(filters);
+
+		return mapping;
 	}
 }
--- src/main/java/com/heisenbug3/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/heisenbug3/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,14 +1,12 @@
 package com.heisenbug3.rest.webservices.restfulwebservices.filtering;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonFilter;
 
+@JsonFilter(("SomeBeanFilter"))
 public class SomeBean {
 
 	private String field1;
 	private String field2;
-
-	@JsonIgnore
 	private String field3;
 
 	public SomeBean(String field1, String field2, String field3) {
